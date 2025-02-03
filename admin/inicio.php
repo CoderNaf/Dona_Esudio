642860f2039e5f28e7e853c425a33bdbb75dfa88
codernaf <produccionesbog@gmail.com>
2025-02-03 15:29:23 -0500

    update components section inicio.php

--- admin/inicio.php
+++ admin/inicio.php
@@ -1,2 +1,60 @@
 <?php  include('./templates/header.php')  ?>
+
+
+<section class="dashboardInitial">
+    <div class="section-heroTitle">
+        <h1>
+            proyect management
+        </h1>
+        <button class="button">
+            ➕ Nuevo Proyecto
+            <div class="hoverEffect">
+                <div></div>
+            </div>
+        </button>
+
+    </div>
+    <section class="info-tables-dates">
+        <article class="sectionInfo-sectionDashboard">
+            <h4></h4>
+            <div>
+                <div>
+                    <h5></h5>
+                    <p></p>
+                </div>
+                <div>
+                    <p>86</p>
+                </div>
+            </div>
+
+        </article>
+        <article class="sectionInfo-sectionDashboard">
+            <h4></h4>
+            <div>
+                <div>
+                    <h5></h5>
+                    <p></p>
+                </div>
+                <div>
+                    <p>86</p>
+                </div>
+            </div>
+
+        </article>
+        <article class="sectionInfo-sectionDashboard">
+            <h4></h4>
+            <div>
+                <div>
+                    <h5></h5>
+                    <p></p>
+                </div>
+                <div>
+                    <p>86</p>
+                </div>
+            </div>
+
+        </article>
+    </section>
+</section>
+
 <?php  include('./templates/footer.php')  ?>
