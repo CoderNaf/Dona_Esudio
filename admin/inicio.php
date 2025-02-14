9eab4faccb9929f68eb89da92e9bfa99b5d5cbaa
FabianSilvaDev <produccionesbog@gmail.com>
2025-02-14 14:06:50 -0500

    create of table projects

--- admin/inicio.php
+++ admin/inicio.php
@@ -57,6 +57,42 @@
     </section>
 </section>
 
+<section class="table-info-projects">
+    <div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Logo</th>
+                    <th scope="col">client name</th>
+                    <th scope="col">project</th>
+                    <th scope="col">status</th>
+                    <th scope="col">update</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">R1C1</td>
+                    <td>R1C2</td>
+                    <td>Lorem, ipsum dolor.</td>
+                    <td>R1C3</td>
+                    <td>R1C3</td>
+                    <td>R1C3</td>
+                </tr>
+                <tr class="">
+                    <td scope="row">Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</section>
+
 
 
 <?php  include('./templates/footer.php')  ?>
